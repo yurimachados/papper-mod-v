e8687377462f68e11f1b51ae40f894deda3232d5
Yuri Machado <ymachado1995@gmail.com>
2023-07-01 15:37:01 -0300

    Exclusão ok

--- assets/js/client-list.js
+++ assets/js/client-list.js
@@ -13,6 +13,19 @@ function createParagraph(className, textContent) {
     return p
 }
 
+function formatDate(dateString) {
+    const date = new Date(dateString);
+
+    const day = String(date.getDay()).padStart(2, '0')
+    const month = String(date.getMonth() + 1).padStart(2, '0')
+    const year = date.getFullYear()
+
+    const hours = String(date.getHours()).padStart(2, '0')
+    const minutes = String(date.getMinutes()).padStart(2, '0')
+
+    return `${day}/${month}/${year} - ${hours}:${minutes}`
+}
+
 function listarClientes() {
     try {
         fetch('http://192.168.0.133:3000/api/client', {
@@ -45,7 +58,7 @@ function listarClientes() {
                         cpf: client.cpf,
                         wpp: client.whatsapp,
                         address: clientAddress,
-                        createdAt: createdAt
+                        createdAt: formatDate(createdAt)
                     }
                     clientList.push(clientData)
                 })
@@ -69,14 +82,32 @@ function createCard(clientData) {
 
         // console.log(client.address)
         const card = document.createElement('div')
-        card.classList.add('client-data', `id_${client.id}`)
+        card.classList.add('client-data', `${client.id}`)
         card.id = `id_${client.id}`
+
+        // Div de opções
+        const optionsDiv = document.createElement('div')
+        optionsDiv.classList.add('options-div')
+
+        // Div de dados
+        const dataDiv = document.createElement('div')
+        dataDiv.classList.add('data-div')
 
         // Botão de exclusão de cliente
         const deleteButton = document.createElement('button');
         deleteButton.textContent = 'X';
         deleteButton.classList.add('delete-button');
         deleteButton.setAttribute('onclick', `excluirCliente(${client.id})`);
+        
+        
+        // Botão de edição de cadastro
+        const editIcon = document.createElement('i')
+        editIcon.classList.add('fas', 'fa-cog')
+
+        const editButton = document.createElement('button');
+        editButton.appendChild(editIcon)
+        editButton.classList.add('edit-button');
+        editButton.setAttribute('onclick', `editarCliente(${client.id})`);
 
         const name = createParagraph('client-name', "Nome: " + client.name)
         const email = createParagraph('client-email', "E-mail: " + client.email)
@@ -85,13 +116,16 @@ function createCard(clientData) {
         const address = createParagraph('client-city', "Endereço: " + client.address)
         const createdAt = createParagraph('client-created-at', "Criado em: " + client.createdAt)
 
-        card.appendChild(deleteButton)
-        card.appendChild(name)
-        card.appendChild(email)
-        card.appendChild(cpf)
-        card.appendChild(wpp)
-        card.appendChild(address)
-        card.appendChild(createdAt)
+        card.appendChild(optionsDiv)
+        card.appendChild(dataDiv)
+        optionsDiv.appendChild(deleteButton)
+        optionsDiv.appendChild(editButton)
+        dataDiv.appendChild(name)
+        dataDiv.appendChild(email)
+        dataDiv.appendChild(cpf)
+        dataDiv.appendChild(wpp)
+        dataDiv.appendChild(address)
+        dataDiv.appendChild(createdAt)
 
         clientItens.appendChild(card);
         console.log(card)
